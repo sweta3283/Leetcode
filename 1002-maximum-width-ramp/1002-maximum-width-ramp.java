59ccdc658be7cdff9e085e7de40b68680cf9e1f5
Sweta M <sweta3283@gmail.com>
2024-10-14 23:18:28 +0530

    Time: 3 ms (99.9%), Space: 55.2 MB (65.65%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.java
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -4,20 +4,19 @@ class Solution {
         int n = nums.length;
         int[] rightMax = new int[n];
         rightMax[n - 1] = nums[n - 1];
-
-        for (int i = n - 2; i >= 0; i--) 
-        rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
-        
-        int left = 0, right = 0, maxVal = 0;
-
-        while (right < n) {
-            while (right<n && nums[left]<=rightMax[right])
-            right++;           
-            maxVal = Math.max(maxVal, right - left-1);
-            left++;
-            right = left + maxVal + 1; 
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
         }
-
-        return maxVal;
+        int left = 0;
+        int right = 0;
+        int maxLen = 0;
+        while (right < n) {
+            while (left < right && (nums[left] > rightMax[right])) {
+                left++;
+            }
+            maxLen = Math.max(maxLen, right - left);
+            right++;
+        }
+        return maxLen;
     }
 }
