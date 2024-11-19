d3ad684ff6ad780ece8c11f454b21f8fc6aefe8b
Sweta M <sweta3283@gmail.com>
2024-11-19 12:42:22 +0530

    Time: 2 ms (27.17%), Space: 42.5 MB (40%) - LeetHub

---
+++ 3421-count-pairs-that-form-a-complete-day-i/3421-count-pairs-that-form-a-complete-day-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int hr : hours) {
+            int rem = hr % 24;
+            int compl = (24 - rem) % 24;
+            if (map.containsKey(compl)) {
+                count = count + map.get(compl);
+            }
+            map.put(rem, map.getOrDefault(rem, 0) + 1);
+            // System.out.println(map);
+        }
+        return count;
+    }
+}
