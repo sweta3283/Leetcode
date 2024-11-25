5dc6310c5ca5393f01be38f98ce22980e289421c
Sweta M <sweta3283@gmail.com>
2024-11-25 23:42:28 +0530

    Time: 1 ms (99%), Space: 40.7 MB (93.14%) - LeetHub

--- 1127-last-stone-weight/1127-last-stone-weight.java
+++ 1127-last-stone-weight/1127-last-stone-weight.java
@@ -7,15 +7,16 @@ class Solution {
         while (pq.size() > 1) {
             int first = pq.poll();
             int sec = pq.poll();
+
             if (first != sec) {
                 sec = first - sec;
                 pq.offer(sec);
             }
         }
-        if (!pq.isEmpty()) {
-            return pq.poll();
-        } else {
+        if (pq.isEmpty()) {
             return 0;
-        }
+        } else
+            return pq.poll();
+
     }
 }
